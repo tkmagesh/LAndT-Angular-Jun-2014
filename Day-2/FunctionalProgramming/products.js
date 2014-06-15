2e5918d94a83f2862133420e547696295c1ab679
tkmagesh <tkmagesh77@gmail.com>
2014-06-15 09:56:13 +0530

    FP - Seed

---
+++ Day-2/FunctionalProgramming/products.js
@@ -0,0 +1,8 @@
+var products = [
+	{id:1,name : "pen", cost :10, units:70, category : 1 },
+	{id:8,name : "hen", cost :90, units:40, category : 2 },
+	{id:3,name : "den", cost :40, units:20, category : 1 },
+	{id:9,name : "len", cost :60, units:70, category : 2 },
+	{id:4,name : "ten", cost :30, units:10, category : 1 },
+	{id:6,name : "ken", cost :20, units:90, category : 2 }
+]
