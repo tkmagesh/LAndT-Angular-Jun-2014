88fc6b6291941dcc4473ef30cce328be623ceb92
tkmagesh <tkmagesh77@gmail.com>
2014-06-15 13:10:37 +0530

    FP Techniques - Complete

--- Day-2/FunctionalProgramming/products.js
+++ Day-2/FunctionalProgramming/products.js
@@ -92,26 +92,111 @@ function min(list,selectorFn){
 	return result;
 }
 
-max
-sum
-avg
-countBy
-any
-all
-groupBy
+function min(list,selector){
+	var selectorFn = typeof selector === "function" ? selector : function(item){ return item[selector]};
+	var result = selectorFn(list[0]);
+	for(var i=0;i<list.length;i++){
+		var value = selectorFn(list[i]);
+		if (value < result) result = value;
+	}
+	return result;	
+}
+
+min(products,function(p){ return p.cost * p.units;})
+
+function min(list,selector){
+	var selectorFn = typeof selector === "function" ? selector : function(item){ return item[selector]};
+	var result = selectorFn(list[0]);
+	for(var i=0;i<list.length;i++){
+		var value = selectorFn(list[i]);
+		if (value > result) result = value;
+	}
+	return result;	
+}
+
+function sum(list,selector){
+	var selectorFn = typeof selector === "function" ? selector : function(item){ return item[selector]};
+	var result = 0;
+	for(var i=0;i<list.length;i++){
+		var value = selectorFn(list[i]);
+		result += value;
+	}
+	return result;	
+}
+
+function avg(list,selector){
+	var result = sum(list,selector);
+	return result/list.length;	
+}
+
+function countBy(list, predicate){
+	var result = 0;
+	for(var i=0;i<list.length;i++)
+		if (predicate(list[i]) === true) ++result;
+	return result;
+}
+function any(list,predicate){
+	for(var i=0;i<list.length;i++)
+		if (predicate(list[i]))
+			return true;
+	return false;
+}
+
+
+function all(list,predicate){
+	for(var i=0;i<list.length;i++)
+		if (!predicate(list[i]))
+			return false;
+	return true;
+}
+
+function groupBy(list,selector){
+	var selectorFn = typeof selector === "function" ? selector : function(item){ return item[selector]};
+	var result = {};
+	for(var i=0;i<list.length;i++){
+		var key = selectorFn(list[i]);
+		result[key] = result[key] || [];
+		result[key].push(list[i]);
+	}
+	return result;
+}
+
+var productsByCategory = groupBy(products,"category");
+
+var productsByCost = groupBy(products,function(p){
+   return p.cost > 50 ? "affordable" : "costly"
+});
 
 var products = [
 	{id:1,name : "pen", cost :10, units:70, category : 1 },
 	{id:8,name : "hen", cost :90, units:40, category : 2 },
-	{id:3,name : "den", cost :40, units:20, category : 1 },
+	{id:3,name : "den", cost :40, units:20, category : 3 },
 	{id:9,name : "len", cost :60, units:70, category : 2 },
 	{id:4,name : "ten", cost :30, units:10, category : 1 },
-	{id:6,name : "ken", cost :20, units:90, category : 2 }
+	{id:6,name : "ken", cost :20, units:90, category : 3 }
 ]
 
 var categories = [
 	{id : 1, name : "stationary"},
-	{id : 2, name : "grocery"}
+	{id : 2, name : "grocery"},
+	{id : 3, name : "utensils"}
 ]
 
-join
+function join(leftList, rightList, keyComparer, transformerFn){
+	var result  = [];
+	for(var i=0;i<leftList.length;i++)
+		for(var j=0;j<rightList.length;j++)
+			if (keyComparer(leftList[i],rightList[j]) === 0)
+				result.push(transformerFn(leftList[i],rightList[j]));
+	return result;
+}
+
+var productsWithCategory = join(products,categories,
+  function(p,c){ 
+      return p.category === c.id ? 0 : (p.category > c.id ? 1 : -1);
+  },
+  function (p,c){
+      return {id : p.id, name : p.name, cost : p.cost, units : p.units, category : c.name};
+  });
+
+console.table(productsWithCategory);
